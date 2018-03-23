c7b1a2d380a8e45702d054865258704efff5b1ec
2153749860 <tiagorocha1515@gmail.com>
2018-03-23 16:00:34 +0000

    ex60

--- ex60.py
+++ ex60.py
@@ -1,4 +1,17 @@
-from math import factorial
-num = int(input('Digite um número para calcular os seu fatorial: '))
+'''from math import factorial
+num = int(input('Digite um número para calcular o seu fatorial: '))
 fat = factorial(num)
-print('O fatorial de {} é igual a {}.'.format(num, fat))
+print('O fatorial de {} é igual a {}.'.format(num, fat))'''
+
+
+
+num = int(input('Digite um número para calcular o seu fatorial: '))
+contador = num       # O contador começa pelo num
+fatorial = 1
+print('Calculando {}! = '.format(num), end='')
+while contador > 0:
+    print('{} '.format(contador), end='')
+    print(' x ' if contador > 1 else ' = ', end='')
+    fatorial *= contador
+    contador -= 1
+print('{}'.format(fatorial))
