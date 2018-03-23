f8323eeb461b460cecfea3603fa9b9deadfc0487
2153749860 <tiagorocha1515@gmail.com>
2018-03-23 14:58:33 +0000

    exercicios

--- ex57.py
+++ ex57.py
@@ -1,3 +1,4 @@
-sexo = 'M' and 'F'
-while sexo != 'M' and sexo != 'F':
-    sexoPessoa = str(input('Digite o seu sexo [M/F]: ')).strip().upper()
+sexo = str(input('Digite o seu sexo [M/F]: ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor digite de novo: ')).strip().upper()[0]
+print('Fim')
---
+++ ex60.py
@@ -0,0 +1,4 @@
+from math import factorial
+num = int(input('Digite um número para calcular os seu fatorial: '))
+fat = factorial(num)
+print('O fatorial de {} é igual a {}.'.format(num, fat))
